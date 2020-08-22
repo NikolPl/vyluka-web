3dadcfc19cbc266124357c1fa05c1fb8c865153e
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-08-22 09:45:53 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -3,7 +3,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("logo").style.width = "50px";
-  } else {
     document.getElementById("logo").style.width = "90px";
+  } else {
   }
 }
+
